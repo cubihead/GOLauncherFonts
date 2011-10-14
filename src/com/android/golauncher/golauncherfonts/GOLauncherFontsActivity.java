3b24999535943e90b041baaa53e3bcc7e54fd08a
beecub <beecub@beecub.com>
2011-10-14 14:59:22 +0200

    - New flexible way to manage the fonts (to add them to the adapter)

--- src/com/android/golauncher/golauncherfonts/GOLauncherFontsActivity.java
+++ src/com/android/golauncher/golauncherfonts/GOLauncherFontsActivity.java
@@ -4,8 +4,10 @@ import java.util.ArrayList;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,7 +18,7 @@ import android.widget.TextView;
 public class GOLauncherFontsActivity extends Activity {
     public static ArrayList<Font> mFonts = new ArrayList<Font>();
     public static FontAdapter madapter;
-    //public static final String LOG_TAG = "beecub";
+    public static final String LOG_TAG = "beecub";
     
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -48,27 +50,45 @@ public class GOLauncherFontsActivity extends Activity {
         TextView tv2 = (TextView) findViewById(R.id.instructions);
         tv2.setText(getString(R.string.instruction));
         
+        
+        // 1 = Name
+        // 2 = License
+        // 3 = Author
+        // 4 = URL
+        // 5 = Category
+        
+        Resources res = getResources();
+        String[] fonts = res.getStringArray(R.array.fonts);
+        
+        int i = 5;
+        while(i < fonts.length) {
+            madapter.add(new Font(fonts[i], fonts[i+1], fonts[i+2], Typeface.createFromAsset(getAssets(), "fonts/" + fonts[i] + ".ttf")));
+            i = i + 5;
+        }
+        
+        
         // fonts
         //madapter.add(new Font(getString(R.string.f0_name), getString(R.string.f0_license), getString(R.string.f0_author), Typeface.createFromAsset(getAssets(), "fonts/" + getString(R.string.f0_name) + ".ttf")));
-        madapter.add(new Font(getString(R.string.f1_name), getString(R.string.f1_license), getString(R.string.f1_author), Typeface.createFromAsset(getAssets(), "fonts/" + getString(R.string.f1_name) + ".ttf")));
-        madapter.add(new Font(getString(R.string.f2_name), getString(R.string.f2_license), getString(R.string.f2_author), Typeface.createFromAsset(getAssets(), "fonts/" + getString(R.string.f2_name) + ".ttf")));
-        madapter.add(new Font(getString(R.string.f3_name), getString(R.string.f3_license), getString(R.string.f3_author), Typeface.createFromAsset(getAssets(), "fonts/" + getString(R.string.f3_name) + ".ttf")));
-        madapter.add(new Font(getString(R.string.f4_name), getString(R.string.f4_license), getString(R.string.f4_author), Typeface.createFromAsset(getAssets(), "fonts/" + getString(R.string.f4_name) + ".ttf")));
-        madapter.add(new Font(getString(R.string.f5_name), getString(R.string.f5_license), getString(R.string.f5_author), Typeface.createFromAsset(getAssets(), "fonts/" + getString(R.string.f5_name) + ".ttf")));
-        madapter.add(new Font(getString(R.string.f6_name), getString(R.string.f6_license), getString(R.string.f6_author), Typeface.createFromAsset(getAssets(), "fonts/" + getString(R.string.f6_name) + ".ttf")));
-        madapter.add(new Font(getString(R.string.f7_name), getString(R.string.f7_license), getString(R.string.f7_author), Typeface.createFromAsset(getAssets(), "fonts/" + getString(R.string.f7_name) + ".ttf")));
-        madapter.add(new Font(getString(R.string.f8_name), getString(R.string.f8_license), getString(R.string.f8_author), Typeface.createFromAsset(getAssets(), "fonts/" + getString(R.string.f8_name) + ".ttf")));
-        madapter.add(new Font(getString(R.string.f9_name), getString(R.string.f9_license), getString(R.string.f9_author), Typeface.createFromAsset(getAssets(), "fonts/" + getString(R.string.f9_name) + ".ttf")));
-        madapter.add(new Font(getString(R.string.f10_name), getString(R.string.f10_license), getString(R.string.f10_author), Typeface.createFromAsset(getAssets(), "fonts/" + getString(R.string.f10_name) + ".ttf")));
-        madapter.add(new Font(getString(R.string.f11_name), getString(R.string.f11_license), getString(R.string.f11_author), Typeface.createFromAsset(getAssets(), "fonts/" + getString(R.string.f11_name) + ".ttf")));
-        madapter.add(new Font(getString(R.string.f12_name), getString(R.string.f12_license), getString(R.string.f12_author), Typeface.createFromAsset(getAssets(), "fonts/" + getString(R.string.f12_name) + ".ttf")));
-        madapter.add(new Font(getString(R.string.f13_name), getString(R.string.f13_license), getString(R.string.f13_author), Typeface.createFromAsset(getAssets(), "fonts/" + getString(R.string.f13_name) + ".ttf")));
-        madapter.add(new Font(getString(R.string.f14_name), getString(R.string.f14_license), getString(R.string.f14_author), Typeface.createFromAsset(getAssets(), "fonts/" + getString(R.string.f14_name) + ".ttf")));
-        madapter.add(new Font(getString(R.string.f15_name), getString(R.string.f15_license), getString(R.string.f15_author), Typeface.createFromAsset(getAssets(), "fonts/" + getString(R.string.f15_name) + ".ttf")));
-        madapter.add(new Font(getString(R.string.f16_name), getString(R.string.f16_license), getString(R.string.f16_author), Typeface.createFromAsset(getAssets(), "fonts/" + getString(R.string.f16_name) + ".ttf")));
-        madapter.add(new Font(getString(R.string.f17_name), getString(R.string.f17_license), getString(R.string.f17_author), Typeface.createFromAsset(getAssets(), "fonts/" + getString(R.string.f17_name) + ".ttf")));
-        madapter.add(new Font(getString(R.string.f18_name), getString(R.string.f18_license), getString(R.string.f18_author), Typeface.createFromAsset(getAssets(), "fonts/" + getString(R.string.f18_name) + ".ttf")));
-        madapter.add(new Font(getString(R.string.f19_name), getString(R.string.f19_license), getString(R.string.f19_author), Typeface.createFromAsset(getAssets(), "fonts/" + getString(R.string.f19_name) + ".ttf")));
+//        madapter.add(new Font(getString(R.string.f1_name), getString(R.string.f1_license), getString(R.string.f1_author), Typeface.createFromAsset(getAssets(), "fonts/" + getString(R.string.f1_name) + ".ttf")));
+//        madapter.add(new Font(getString(R.string.f2_name), getString(R.string.f2_license), getString(R.string.f2_author), Typeface.createFromAsset(getAssets(), "fonts/" + getString(R.string.f2_name) + ".ttf")));
+//        madapter.add(new Font(getString(R.string.f3_name), getString(R.string.f3_license), getString(R.string.f3_author), Typeface.createFromAsset(getAssets(), "fonts/" + getString(R.string.f3_name) + ".ttf")));
+//        madapter.add(new Font(getString(R.string.f4_name), getString(R.string.f4_license), getString(R.string.f4_author), Typeface.createFromAsset(getAssets(), "fonts/" + getString(R.string.f4_name) + ".ttf")));
+//        madapter.add(new Font(getString(R.string.f5_name), getString(R.string.f5_license), getString(R.string.f5_author), Typeface.createFromAsset(getAssets(), "fonts/" + getString(R.string.f5_name) + ".ttf")));
+//        madapter.add(new Font(getString(R.string.f6_name), getString(R.string.f6_license), getString(R.string.f6_author), Typeface.createFromAsset(getAssets(), "fonts/" + getString(R.string.f6_name) + ".ttf")));
+//        madapter.add(new Font(getString(R.string.f7_name), getString(R.string.f7_license), getString(R.string.f7_author), Typeface.createFromAsset(getAssets(), "fonts/" + getString(R.string.f7_name) + ".ttf")));
+//        madapter.add(new Font(getString(R.string.f8_name), getString(R.string.f8_license), getString(R.string.f8_author), Typeface.createFromAsset(getAssets(), "fonts/" + getString(R.string.f8_name) + ".ttf")));
+//        madapter.add(new Font(getString(R.string.f9_name), getString(R.string.f9_license), getString(R.string.f9_author), Typeface.createFromAsset(getAssets(), "fonts/" + getString(R.string.f9_name) + ".ttf")));
+//        madapter.add(new Font(getString(R.string.f10_name), getString(R.string.f10_license), getString(R.string.f10_author), Typeface.createFromAsset(getAssets(), "fonts/" + getString(R.string.f10_name) + ".ttf")));
+//        madapter.add(new Font(getString(R.string.f11_name), getString(R.string.f11_license), getString(R.string.f11_author), Typeface.createFromAsset(getAssets(), "fonts/" + getString(R.string.f11_name) + ".ttf")));
+//        madapter.add(new Font(getString(R.string.f12_name), getString(R.string.f12_license), getString(R.string.f12_author), Typeface.createFromAsset(getAssets(), "fonts/" + getString(R.string.f12_name) + ".ttf")));
+//        madapter.add(new Font(getString(R.string.f13_name), getString(R.string.f13_license), getString(R.string.f13_author), Typeface.createFromAsset(getAssets(), "fonts/" + getString(R.string.f13_name) + ".ttf")));
+//        madapter.add(new Font(getString(R.string.f14_name), getString(R.string.f14_license), getString(R.string.f14_author), Typeface.createFromAsset(getAssets(), "fonts/" + getString(R.string.f14_name) + ".ttf")));
+//        madapter.add(new Font(getString(R.string.f15_name), getString(R.string.f15_license), getString(R.string.f15_author), Typeface.createFromAsset(getAssets(), "fonts/" + getString(R.string.f15_name) + ".ttf")));
+//        madapter.add(new Font(getString(R.string.f16_name), getString(R.string.f16_license), getString(R.string.f16_author), Typeface.createFromAsset(getAssets(), "fonts/" + getString(R.string.f16_name) + ".ttf")));
+//        madapter.add(new Font(getString(R.string.f17_name), getString(R.string.f17_license), getString(R.string.f17_author), Typeface.createFromAsset(getAssets(), "fonts/" + getString(R.string.f17_name) + ".ttf")));
+//        madapter.add(new Font(getString(R.string.f18_name), getString(R.string.f18_license), getString(R.string.f18_author), Typeface.createFromAsset(getAssets(), "fonts/" + getString(R.string.f18_name) + ".ttf")));
+//        madapter.add(new Font(getString(R.string.f19_name), getString(R.string.f19_license), getString(R.string.f19_author), Typeface.createFromAsset(getAssets(), "fonts/" + getString(R.string.f19_name) + ".ttf")));
+        
     }
     
     public class FontAdapter extends ArrayAdapter<Font> {
